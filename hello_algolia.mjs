dadc45e3c90e2fe632a9e82e14a4f0d5d837befb
Alexis <alexis.flacher@mezcalito.fr>
2024-09-30 16:41:50 +0200

    add env key

--- hello_algolia.mjs
+++ hello_algolia.mjs
@@ -1,7 +1,7 @@
 import {algoliasearch} from "algoliasearch";
 
-const appID = "4JZM1RK86D";
-const apiKey = "dc580b87b8cab44f44bc47cf3c442d91";
+const appID = import.meta.env.VITE_ALGOLIA_APP;
+const apiKey = import.meta.env.VITE_ALGOLIA_API;
 const indexName = "movies";
 
 let data = [];
@@ -11,7 +11,7 @@ const client = algoliasearch(appID, apiKey);
 
 try {
     const response = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=fr-FR&page=1&region=FR', {
-        headers: {Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmUxZTYyMjlhMzIzYWI2Yjc5YWExMTZlNjAxZmY1NiIsIm5iZiI6MTcyNTk3NzI2Ny4wNTUyMTUsInN1YiI6IjYxNzE2MTJiMWQwMTkxMDA0NDFiMjQzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.am3mQMPjEmient4GJAl8eepEATLbo7_znzFqUnTZ_MI`}
+        headers: {Authorization: `Bearer ${import.meta.env.VITE_TMBD_TOKEN}`}
     });
     const res = await response.json()
     data = res.results
@@ -27,7 +27,7 @@ if (data && total_page) {
     for (let i = 1; i < total_page; i++) {
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?language=fr-FR&page=${i + 1}&region=FR`, {
-                headers: {Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmUxZTYyMjlhMzIzYWI2Yjc5YWExMTZlNjAxZmY1NiIsIm5iZiI6MTcyNTk3NzI2Ny4wNTUyMTUsInN1YiI6IjYxNzE2MTJiMWQwMTkxMDA0NDFiMjQzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.am3mQMPjEmient4GJAl8eepEATLbo7_znzFqUnTZ_MI`}
+                headers: {Authorization: `Bearer ${import.meta.env.VITE_TMBD_TOKEN}`}
             });
             const res = await response.json()
             data.push(...res.results)
