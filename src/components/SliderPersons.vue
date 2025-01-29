8ea1a3802c0b5dfb79bf133ce80791099a57d42c
AlexisF9 <alexis.flacher38@gmail.com>
2025-01-29 16:49:24 +0100

    bug fix duplicates person in slider persons

--- src/components/SliderPersons.vue
+++ src/components/SliderPersons.vue
@@ -1,32 +1,41 @@
 <template>
   <div v-if="list">
+    {{ console.log(list) }}
     <div class="c-slider-persons__intro">
       <h2 class="c-h-xl u-text-white">{{ title }}</h2>
     </div>
     <Swiper
-        class="c-slider-persons__swiper"
-        slides-per-view="auto"
-        :space-between="20"
-        :breakpoints="{
-          '640': {
-            slidesPerView: 2,
-          },
-          '768': {
-            slidesPerView: 3,
-          },
-          '1024': {
-            slidesPerView: 5,
-          },
-        }"
-        :modules="[Navigation]"
-        :navigation="{enabled: true, prevEl: '.person-prev', nextEl: '.person-next'}"
+      class="c-slider-persons__swiper"
+      slides-per-view="auto"
+      :space-between="20"
+      :breakpoints="{
+        '640': {
+          slidesPerView: 2,
+        },
+        '768': {
+          slidesPerView: 3,
+        },
+        '1024': {
+          slidesPerView: 5,
+        },
+      }"
+      :modules="[Navigation]"
+      :navigation="{
+        enabled: true,
+        prevEl: '.person-prev',
+        nextEl: '.person-next',
+      }"
     >
-      <SwiperSlide v-for="person in list">
+      <SwiperSlide v-for="person in mergeJobsById(list)">
         <PersonCard :person="person" />
       </SwiperSlide>
       <div class="c-slider-persons__nav">
-        <button class="u-text-white person-prev"><i class="fa-solid fa-chevron-left"></i></button>
-        <button class="u-text-white person-next"><i class="fa-solid fa-chevron-right"></i></button>
+        <button class="u-text-white person-prev">
+          <i class="fa-solid fa-chevron-left"></i>
+        </button>
+        <button class="u-text-white person-next">
+          <i class="fa-solid fa-chevron-right"></i>
+        </button>
       </div>
     </Swiper>
   </div>
@@ -34,10 +43,31 @@
 
 <script setup lang="ts">
 import { Navigation } from "swiper/modules";
-import {Swiper, SwiperSlide} from "swiper/vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 import PersonCard from "./PersonCard.vue";
 
-defineProps(['title', 'subtitle', 'list'])
+defineProps(["title", "subtitle", "list"]);
+
+const mergeJobsById = (data: any) => {
+  const seenIds = new Set();
+  return data.reduce((acc: any, item: any) => {
+    if (!seenIds.has(item.id)) {
+      // Si l'ID n'a pas encore été vu, on l'ajoute à l'accumulateur
+      seenIds.add(item.id);
+      acc.push(item);
+    } else {
+      // Si l'ID a déjà été rencontré, on fusionne le job avec l'objet existant
+      const existingItem = acc.find(
+        (obj: { id: Number }) => obj.id === item.id
+      );
+      // Vérifier si le job est déjà présent avant de l'ajouter
+      if (!existingItem.job.includes(item.job)) {
+        existingItem.job += ` / ${item.job}`;
+      }
+    }
+    return acc;
+  }, []);
+};
 </script>
--- src/pages/SinglePerson.vue
+++ src/pages/SinglePerson.vue
@@ -2,128 +2,215 @@
   <div class="o-container" v-if="person">
     <div class="c-single-person__infos">
       <div class="c-single-person__intro">
-        <img v-if="person.profile_path" class="c-single-person__picture" :src="`https://image.tmdb.org/t/p/w300${person.profile_path}`" :alt="`Poster ${person.name}`" />
+        <img
+          v-if="person.profile_path"
+          class="c-single-person__picture"
+          :src="`https://image.tmdb.org/t/p/w300${person.profile_path}`"
+          :alt="`Poster ${person.name}`"
+        />
         <div>
           <h2 class="c-h-2xl u-text-white u-mb-16">{{ person.name }}</h2>
-          <p class="c-text-l u-text-white" v-if="person.place_of_birth">Natif de {{ person.place_of_birth }}</p>
-          <p class="c-text-l u-text-white" v-if="person.birthday">Né le {{ getDate(person.birthday )}}</p>
-          <p class="c-text-l u-text-white" v-if="person.birthday && person.deathday">Mort le {{ getDate(person.deathday)}}</p>
-          <p class="c-text-l u-text-white" v-if="person.known_for_department">Connu pour {{ person.known_for_department }}</p>
+          <p class="c-text-l u-text-white" v-if="person.place_of_birth">
+            Natif de {{ person.place_of_birth }}
+          </p>
+          <p class="c-text-l u-text-white" v-if="person.birthday">
+            Né le {{ getDate(person.birthday) }}
+          </p>
+          <p
+            class="c-text-l u-text-white"
+            v-if="person.birthday && person.deathday"
+          >
+            Mort le {{ getDate(person.deathday) }}
+          </p>
+          <p class="c-text-l u-text-white" v-if="person.known_for_department">
+            Connu pour {{ person.known_for_department }}
+          </p>
           <div v-if="socials" class="c-single-person__socials">
-            <a class="c-text-l" target="_blank" v-if="socials.instagram_id" :href="`https://www.instagram.com/${socials.instagram_id}/`"><i class="fab fa-instagram"></i></a>
-            <a class="c-text-l" target="_blank" v-if="socials.tiktok_id" :href="`https://www.tiktok.com/@${socials.tiktok_id}`"><i class="fab fa-tiktok"></i></a>
-            <a class="c-text-l" target="_blank" v-if="socials.twitter_id" :href="`https://x.com/${socials.twitter_id}`"><i class="fab fa-twitter"></i></a>
-            <a class="c-text-l" target="_blank" v-if="socials.youtube_id" :href="`https://www.youtube.com/@${socials.youtube_id}`"><i class="fab fa-youtube"></i></a>
+            <a
+              class="c-text-l"
+              target="_blank"
+              v-if="socials.instagram_id"
+              :href="`https://www.instagram.com/${socials.instagram_id}/`"
+              ><i class="fab fa-instagram"></i
+            ></a>
+            <a
+              class="c-text-l"
+              target="_blank"
+              v-if="socials.tiktok_id"
+              :href="`https://www.tiktok.com/@${socials.tiktok_id}`"
+              ><i class="fab fa-tiktok"></i
+            ></a>
+            <a
+              class="c-text-l"
+              target="_blank"
+              v-if="socials.twitter_id"
+              :href="`https://x.com/${socials.twitter_id}`"
+              ><i class="fab fa-twitter"></i
+            ></a>
+            <a
+              class="c-text-l"
+              target="_blank"
+              v-if="socials.youtube_id"
+              :href="`https://www.youtube.com/@${socials.youtube_id}`"
+              ><i class="fab fa-youtube"></i
+            ></a>
           </div>
         </div>
       </div>
     </div>
 
-    <h2 class="c-h-xl u-text-white u-mt-32 u-mb-16" v-if="person.biography">Biographie</h2>
-    <p class="c-text-m u-text-white" v-if="person.biography">{{ person.biography }}</p>
+    <h2 class="c-h-xl u-text-white u-mt-32 u-mb-16" v-if="person.biography">
+      Biographie
+    </h2>
+    <p class="c-text-m u-text-white" v-if="person.biography">
+      {{ person.biography }}
+    </p>
 
     <div class="c-single-person__sliders">
       <SliderTabsMovies
-          v-if="filtered_movies?.cast && filtered_movies.cast.length > 0 || filtered_tv?.cast && filtered_tv.cast.length > 0"
-          title="Au casting de"
-          :tabs="[
-                { value: 'movie', label: 'Film', list: filtered_movies?.cast ? filtered_movies.cast : [] },
-                { value: 'tv', label: 'Série', list: filtered_tv?.cast ? filtered_tv.cast : [] }
-            ]"
-          :type="filtered_movies?.cast && filtered_movies.cast.length === 0 ? trendingType = 'tv' : trendingType2"
-          v-model:select="trendingType2"
+        v-if="
+          (filtered_movies?.cast && filtered_movies.cast.length > 0) ||
+          (filtered_tv?.cast && filtered_tv.cast.length > 0)
+        "
+        title="Au casting de"
+        :tabs="[
+          {
+            value: 'movie',
+            label: 'Film',
+            list: filtered_movies?.cast ? filtered_movies.cast : [],
+          },
+          {
+            value: 'tv',
+            label: 'Série',
+            list: filtered_tv?.cast ? filtered_tv.cast : [],
+          },
+        ]"
+        :type="
+          filtered_movies?.cast && filtered_movies.cast.length === 0
+            ? (trendingType = 'tv')
+            : trendingType2
+        "
+        v-model:select="trendingType2"
       />
 
       <SliderTabsMovies
-          v-if="filtered_movies?.crew && filtered_movies.crew.length > 0 || filtered_tv?.crew && filtered_tv.crew.length > 0"
-          title="Dans l'équipe de"
-          :tabs="[
-                { value: 'movie', label: 'Film', list: filtered_movies?.crew ? filtered_movies.crew : [] },
-                { value: 'tv', label: 'Série', list: filtered_tv?.crew ? filtered_tv.crew : [] }
-            ]"
-          :type="filtered_movies?.crew && filtered_movies.crew.length === 0 ? trendingType = 'tv' : trendingType"
-          v-model:select="trendingType"
+        v-if="
+          (filtered_movies?.crew && filtered_movies.crew.length > 0) ||
+          (filtered_tv?.crew && filtered_tv.crew.length > 0)
+        "
+        title="Dans l'équipe de"
+        :tabs="[
+          {
+            value: 'movie',
+            label: 'Film',
+            list: filtered_movies?.crew ? filtered_movies.crew : [],
+          },
+          {
+            value: 'tv',
+            label: 'Série',
+            list: filtered_tv?.crew ? filtered_tv.crew : [],
+          },
+        ]"
+        :type="
+          filtered_movies?.crew && filtered_movies.crew.length === 0
+            ? (trendingType = 'tv')
+            : trendingType
+        "
+        v-model:select="trendingType"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useRoute} from "vue-router";
-import {ref, watch, watchEffect} from "vue";
+import { useRoute } from "vue-router";
+import { ref, watch, watchEffect } from "vue";
 import SliderTabsMovies from "../components/SliderTabsMovies.vue";
 
-const route = useRoute()
-const trendingType = ref('movie')
-const trendingType2 = ref('movie')
+const route = useRoute();
+const trendingType = ref("movie");
+const trendingType2 = ref("movie");
 
-const person = ref<{profile_path: string, name: string, birthday: string, deathday: string, place_of_birth: string, biography: string, known_for_department: string}>()
-const movies = ref<{cast?: any[], crew?: any[]}>()
-const filtered_movies = ref<{cast?: any[], crew?: any[]}>({cast: [], crew: []})
-const tv = ref<{cast?: any[], crew?: any[]}>()
-const filtered_tv = ref<{cast?: any[], crew?: any[]}>({cast: [], crew: []})
-const socials = ref<{instagram_id?: string, tiktok_id?: string, twitter_id?: string, youtube_id?: string}>()
+const person = ref<{
+  profile_path: string;
+  name: string;
+  birthday: string;
+  deathday: string;
+  place_of_birth: string;
+  biography: string;
+  known_for_department: string;
+}>();
+const movies = ref<{ cast?: any[]; crew?: any[] }>();
+const filtered_movies = ref<{ cast?: any[]; crew?: any[] }>({
+  cast: [],
+  crew: [],
+});
+const tv = ref<{ cast?: any[]; crew?: any[] }>();
+const filtered_tv = ref<{ cast?: any[]; crew?: any[] }>({ cast: [], crew: [] });
+const socials = ref<{
+  instagram_id?: string;
+  tiktok_id?: string;
+  twitter_id?: string;
+  youtube_id?: string;
+}>();
 
-const fetchData = async(url: string, elem: any) => {
-  elem.value = null
+const fetchData = async (url: string, elem: any) => {
+  elem.value = null;
 
   try {
-    const response = await fetch(`${import.meta.env.VITE_TMBD_URL}/person/${route.params.id}` + url, {
-      headers: {Authorization: `Bearer ${import.meta.env.VITE_TMBD_TOKEN}`}
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_TMBD_URL}/person/${route.params.id}` + url,
+      {
+        headers: { Authorization: `Bearer ${import.meta.env.VITE_TMBD_TOKEN}` },
+      }
+    );
     elem.value = await response.json();
   } catch (err: any) {
-    console.log(err.toString())
+    console.log(err.toString());
   }
-}
+};
 
 watchEffect(() => {
-  fetchData(`?language=fr-FR`, person)
-  fetchData(`/movie_credits?language=fr-FR`, movies)
-  fetchData(`/tv_credits?language=fr-FR`, tv)
-  fetchData(`/external_ids`, socials)
-})
+  fetchData(`?language=fr-FR`, person);
+  fetchData(`/movie_credits?language=fr-FR`, movies);
+  fetchData(`/tv_credits?language=fr-FR`, tv);
+  fetchData(`/external_ids`, socials);
+});
 
 const getDate = (date: string) => {
-  const newDate = new Date(date)
-  return newDate.toLocaleDateString()
-}
+  const newDate = new Date(date);
+  return newDate.toLocaleDateString();
+};
 
 watch(movies, () => {
-    if(movies.value) {
-      movies?.value?.crew?.forEach((el:any) => {
-        if(!filtered_movies?.value?.crew?.find((e: any) => e.id === el.id)) {
-          filtered_movies?.value?.crew?.push(el)
-        }
-      })
+  if (movies.value) {
+    movies?.value?.crew?.forEach((el: any) => {
+      if (!filtered_movies?.value?.crew?.find((e: any) => e.id === el.id)) {
+        filtered_movies?.value?.crew?.push(el);
+      }
+    });
 
-      movies?.value?.cast?.forEach((el:any) => {
-        if(!filtered_movies?.value?.cast?.find((e: any) => e.id === el.id)) {
-          filtered_movies?.value?.cast?.push(el)
-        }
-      })
-    }
-})
+    movies?.value?.cast?.forEach((el: any) => {
+      if (!filtered_movies?.value?.cast?.find((e: any) => e.id === el.id)) {
+        filtered_movies?.value?.cast?.push(el);
+      }
+    });
+  }
+});
 
 watch(tv, () => {
-  if(tv.value) {
-    tv?.value?.crew?.forEach((el:any) => {
-      if(!filtered_tv?.value?.crew?.find((e: any) => e.id === el.id)) {
-        filtered_tv?.value?.crew?.push(el)
+  if (tv.value) {
+    tv?.value?.crew?.forEach((el: any) => {
+      if (!filtered_tv?.value?.crew?.find((e: any) => e.id === el.id)) {
+        filtered_tv?.value?.crew?.push(el);
       }
-    })
+    });
 
-    tv?.value?.cast?.forEach((el:any) => {
-      if(!filtered_tv?.value?.cast?.find((e: any) => e.id === el.id)) {
-        filtered_tv?.value?.cast?.push(el)
+    tv?.value?.cast?.forEach((el: any) => {
+      if (!filtered_tv?.value?.cast?.find((e: any) => e.id === el.id)) {
+        filtered_tv?.value?.cast?.push(el);
       }
-    })
+    });
   }
-})
-//const filteredMoviesDates = (array: any) => {
-//  return array.sort((a:any, b:any) => new Date(b.release_date).getTime() - new Date(a.release_date).getTime())
-//}
-//const filteredTvDates = (array: any) => {
-//  return array.sort((a:any, b:any) => new Date(b.first_air_date).getTime() - new Date(a.first_air_date).getTime())
-//}
+});
 </script>
